4276c951fdcae6f38f2d7fbfad296a51d22ec1ce
leomelki <melkileo@gmail.com>
2022-06-22 19:29:19 +0200

    opti(compress): opti compression + fix crash on odd video dimensions

--- video_processor.py
+++ video_processor.py
@@ -19,34 +19,16 @@ class VideoProcessor:
             f'{key_path}.{dest_ext or key_ext}'
         )
 
-        self.client.upload_file(
-            self.__generate_thumbnail(temp_video),
-            self.bucket,
-            f'{key_path}.jpg'
-        )
-
     @staticmethod
     def __compress(path, ext):
         print('[INFO] compressing video...')
         out_filename = tempfile._get_default_tempdir() + os.path.sep + next(tempfile._get_candidate_names()) + "." + ext
 
         sp = subprocess.run(
-            shlex.split(f"/opt/bin/ffmpeg -i {path} -vf scale=-1:720 -vcodec h264 -acodec aac {out_filename}"),
+            shlex.split(f"/opt/bin/ffmpeg -i {path} -vcodec h264 -crf 28 -preset faster -c:a aac -b:a 96k -movflags +faststart -vf scale=-2:720 -profile:v baseline -preset veryfast -fpsmax 50 -ar 44100 {out_filename}"),
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE
         )
 
 
-        return out_filename
-
-    @staticmethod
-    def __generate_thumbnail(path):
-        print('[INFO] generating video thumbnail...')
-        out_filename = tempfile._get_default_tempdir() + os.path.sep + next(tempfile._get_candidate_names()) + ".jpg"
-
-        sp = subprocess.run(
-            shlex.split(f'/opt/bin/ffmpeg -i {path} -ss 00:00:02 -vframes 1 -vf scale=-1:720 {out_filename}'),
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
         return out_filename
