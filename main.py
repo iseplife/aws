388fb0f707aacc72e7986d0f826dffa37fe764f2
Paul Vénard <dev.warthy@gmail.com>
2022-07-16 08:35:17 +0000

    fix ident

--- main.py
+++ main.py
@@ -28,43 +28,43 @@ def handler(event, context):
             obj = s3_client.get_object(Bucket=bucket, Key=key)
             if obj["Metadata"].get("process", 0):
                 try:
-                # Mark media as being processed
-                cur = conn.cursor()
-                cur.execute("UPDATE media SET status ='PROCESSING' WHERE name=%s", (key,))
-                conn.commit()
-
-                try:
-                    s3_source_signed_url = s3_client.generate_presigned_url(
-                        'get_object',
-                        Params={'Bucket': bucket, 'Key': key},
-                        ExpiresIn=SIGNED_URL_TIMEOUT
-                    )
-
-                    # Videos are all stored in 'vid/' folder in S3 so if this part is in the key (pathname) then it is a video
-                    # otherwise we considered it is a image. Documents are not processed as they don't have the 'process' metadata (yet ?)
-                    if "vid/" in key:
-                        processor = VideoProcessor(s3_client, bucket)
-                    else:
-                        processor = ImageProcessor(s3_client, bucket)
-
-                    print('[INFO] processing object {}...'.format(key))
-                    processor.process(
-                        s3_source_signed_url,
-                        obj["Metadata"],
-                        key,
-                        obj["Metadata"].get("dest_ext", None)
-                    )
-                
-                    # Mark media as ready after processing
+                    # Mark media as being processed
                     cur = conn.cursor()
-                    cur.execute("UPDATE media SET status = 'READY' WHERE name=%s", (key,))
+                    cur.execute("UPDATE media SET status ='PROCESSING' WHERE name=%s", (key,))
                     conn.commit()
-                except Exception as e:
-                    print('Error raised when trying to process object {} from bucket {}.'.format(key, bucket))
-                    cur.execute("UPDATE media SET status = 'ERROR' WHERE name=%s", (key,))
-                    conn.commit()
-                    print(e)
-                    raise e
+
+                    try:
+                        s3_source_signed_url = s3_client.generate_presigned_url(
+                            'get_object',
+                            Params={'Bucket': bucket, 'Key': key},
+                            ExpiresIn=SIGNED_URL_TIMEOUT
+                        )
+
+                        # Videos are all stored in 'vid/' folder in S3 so if this part is in the key (pathname) then it is a video
+                        # otherwise we considered it is a image. Documents are not processed as they don't have the 'process' metadata (yet ?)
+                        if "vid/" in key:
+                            processor = VideoProcessor(s3_client, bucket)
+                        else:
+                            processor = ImageProcessor(s3_client, bucket)
+
+                        print('[INFO] processing object {}...'.format(key))
+                        processor.process(
+                            s3_source_signed_url,
+                            obj["Metadata"],
+                            key,
+                            obj["Metadata"].get("dest_ext", None)
+                        )
+                    
+                        # Mark media as ready after processing
+                        cur = conn.cursor()
+                        cur.execute("UPDATE media SET status = 'READY' WHERE name=%s", (key,))
+                        conn.commit()
+                    except Exception as e:
+                        print('Error raised when trying to process object {} from bucket {}.'.format(key, bucket))
+                        cur.execute("UPDATE media SET status = 'ERROR' WHERE name=%s", (key,))
+                        conn.commit()
+                        print(e)
+                        raise e
                 except OperationalError as e:
                     print("OperationalError with psycopg when updating media status in database (name: {})".format(key))
                     print(e)
